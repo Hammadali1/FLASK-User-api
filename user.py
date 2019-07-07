8a50c14b0abbc9a2dbbee8c3e16f9b3255bd6fc4
Hammadali1 <hammadali298354@gmail.com>
2019-07-07 21:40:38 +0500

    Update user.py

--- user.py
+++ user.py
@@ -13,6 +13,77 @@ mongo = PyMongo(app)
 
 
 
+@app.route("/Admin",methods=['GET'])
+def ShowAdmin():
+    if request.method=='GET':
+       data=mongo.db.admin.find({}).count()
+       if(data==0):
+           return "data not found"
+       else:
+            d = []
+            data=mongo.db.admin.find({})
+            for i in data:
+                d.append({"id":i["_id"] ,"name": i["uname"],"email":i["email"]})
+            return (jsonify(d))
+                  
+    #return render_template('signup.html')
+
+@app.route("/Admin/<string:param>",methods=['GET','POST'])
+def UpdateAdmin(param):
+        
+      if request.method=='GET':
+        print("enter")
+        if (mongo.db.admin.find({'_id': param})):
+             d = []
+             data=mongo.db.admin.find({'_id':param})
+             for i in data:
+                 d.append({"id":i["_id"] ,"name": i["uname"],"email":i["email"]})
+             return jsonify(d)
+        return ('not find')           
+
+      
+      if request.method=='POST':
+        Uname=request.form['name']
+        Id=param
+        Message=request.form['email']
+        Oldpassword=request.form['oldPassword']
+        Newpassword=request.form['newPassword']
+
+
+        d={}
+        data=mongo.db.admin.find({'_id': param})
+        for i in data:
+          dd={"id":i["_id"] ,"name": i["uname"],"email":i["email"],'password':i["password"]}  
+        password=dd['password']
+        if (Oldpassword==password):
+
+            if (mongo.db.admin.find({'_id': param})):
+        
+              update_query=mongo.db.admin.update_one({"_id": Id},{'$set':{"uname":Uname,"email": Message,"password":Newpassword}})
+          
+            return ("Successfully add")   
+        else:
+           return ("wrong password")           
+    
+    #elif request.method=='PUT':
+
+
+
+@app.route("/Admin/Login",methods=['POST'])
+def LoginAdmin():
+    if request.method=='POST':
+      
+      login_user = mongo.db.admin.find_one({'email' : request.form['Email']})
+
+      if login_user:
+          if (request.form['Password'] == login_user['password']):
+            
+               return ("Successfully add") 
+            #return render_template('adminpage.html')
+          return jsonify("False")   
+      return jsonify("False")
+    #return render_template('login.html')
+
 
 
 
