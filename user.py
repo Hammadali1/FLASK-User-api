df894dddaadd80a9514192ca1336312c63c49dcf
Hammadali1 <hammadali298354@gmail.com>
2019-07-31 20:54:33 +0500

    Update user.py

--- user.py
+++ user.py
@@ -1,11 +1,12 @@
     
 from flask_pymongo import PyMongo
 from flask import Flask, jsonify,json,request,render_template
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 app.config['MONGO_DBNAME']=''
 app.config['MONGO_URI']='mongodb://hammad:hammad123@ds351455.mlab.com:51455/data'
